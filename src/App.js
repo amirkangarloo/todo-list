d53e5c25f47b40f2be9282216e0db869ad49ff0c
Amir kangarloo <amirkangarloo1051@gmail.com>
2024-01-30 17:31:10 +0330

    feat: Create component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import TodoWrapper from './Components/TodoWrapper';
 
 function App() {
   return (
-    <>
-      APP
-    </>
+    <div className='App'>
+      <TodoWrapper />
+    </div>
   );
 }
 
---
+++ src/Components/TodoEditeForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TodoEditeForm() {
+  return (
+    <div>TodoEditeForm</div>
+  )
+}
